577afbf432aed1fa85afffe0905e206c9b9986a3
Lisa Marie McGerr <lisamarie.mcgerr@gmail.com>
2020-11-29 19:23:17 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -37,5 +37,5 @@ end
   #:lives points to a hash of value (string) "Subway", "Central Park", "Library", "City Hall"
     #"Subway" points to an array of (string) "Theo", "Queenie"
     #"Central Park" points to an array of (string) "Alex", "Ms. K", "Lucky"
-    #"Library" points to an array of (string) 
+    #"Library" points to an array of (string) "Peter Jr."
     #"City Hall" points to an array of (string)
