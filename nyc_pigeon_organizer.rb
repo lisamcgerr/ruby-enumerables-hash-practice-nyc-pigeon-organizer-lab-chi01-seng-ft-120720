30217a3fc8b83189f6e5cfbb88b3fd458c1291da
Lisa Marie McGerr <lisamarie.mcgerr@gmail.com>
2020-11-29 19:07:43 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -6,7 +6,15 @@ def nyc_pigeon_organizer(data)
   data.each_with_object({ }) do |(key, value), final_array|
     
     value.each do |inner_key, names|
+      names.each do |name|
       binding.pry
+      if !final_array[name]
+        
+      end
+    end
+  end
+  final_array
+end
   
   binding.pry
 end
