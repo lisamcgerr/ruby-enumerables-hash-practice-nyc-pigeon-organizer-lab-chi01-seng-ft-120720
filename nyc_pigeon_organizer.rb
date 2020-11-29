a455eca9f8d7c52d6e5e5532f00697c9ec201b9a
Lisa Marie McGerr <lisamarie.mcgerr@gmail.com>
2020-11-29 19:46:06 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -3,12 +3,12 @@ require 'pry'
 def nyc_pigeon_organizer(data)
   #data is the original hash
   
-  data.each_with_object({ }) do |(key, value), final_array|
+  data.each_with_object({ }) do |(key, value), final_hash|
     
     value.each do |inner_key, names|
       names.each do |name|
       binding.pry
-      if !final_array[name]
+      if !final_hash[name]
         
       end
     end
